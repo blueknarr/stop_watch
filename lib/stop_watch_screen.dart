0531f184749fa0e04fa770c96d3f60727f69bd5c
blueknarr <knarrblue@gmail.com>
2023-06-14 19:35:00 +0900

    067. 시작, 일시정ㅇ지 버튼 UI 작성

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -10,6 +10,33 @@ class StopWatchScreen extends StatefulWidget {
 class _StopWatchScreenState extends State<StopWatchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스톱워치'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.orange,
+                onPressed: () {},
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
