ea566868f45f45cab79ebead17b62a7e5f150c4c
blueknarr <knarrblue@gmail.com>
2023-06-14 20:14:36 +0900

    074. 시작, 일시정지 동작 로직, 시간표시 UI 수정

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -26,9 +26,17 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
     }
   }
 
-  void _start() {}
+  void _start() {
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
 
-  void _pause() {}
+  void _pause() {
+    _timer?.cancel();
+  }
 
   @override
   void dispose() {
@@ -38,6 +46,9 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
 
   @override
   Widget build(BuildContext context) {
+    int sec = _time ~/ 100;
+    String hundredth = '${_time % 100}'.padLeft(2, '0');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('스톱워치'),
@@ -45,16 +56,16 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
       body: Column(
         children: [
           const SizedBox(height: 30),
-          const Row(
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Text(
-                '0',
+                '$sec',
                 style: TextStyle(fontSize: 50),
               ),
               Text(
-                '00',
+                hundredth,
               ),
             ],
           ),
